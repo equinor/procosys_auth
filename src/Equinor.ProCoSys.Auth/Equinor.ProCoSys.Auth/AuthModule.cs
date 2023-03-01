5da9c3217915cf1f13db96c41e558e9a6f5b01c7
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2023-03-01 21:38:28 +0100

    100793 - Pipeline and initial minor solution (#1)
    * Initial minimum solution

---
+++ src/Equinor.ProCoSys.Auth/Equinor.ProCoSys.Auth/AuthModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Equinor.ProCoSys.Auth
+{
+    public static class AuthModule
+    {
+        public static IServiceCollection AddPcsAuthIntegration(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
