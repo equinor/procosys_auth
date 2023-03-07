3cb4d92a365d99d882d402a76da86ba8526963af
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-07 09:44:45 +0100

    More Common code

---
+++ src/Equinor.ProCoSys.Common/Email/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.Common.Email
+{
+    public interface IEmailService
+    {
+        Task SendEmailsAsync(List<string> emails, string subject, string body, CancellationToken token = default);
+    }
+}
