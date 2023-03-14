93010d0c8fee4db359c9f63cc09fe5d0bec6323e
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-14 11:20:44 +0100

    Code from preservation

--- src/Equinor.ProCoSys.Common/Misc/DateTimeExtensions.cs
+++ src/Equinor.ProCoSys.Common/Misc/DateTimeExtensions.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Equinor.ProCoSys.Common.Misc
 {
     public static class DateTimeExtensions
     {
-        private static readonly DayOfWeek firstDayOfWeek = DayOfWeek.Monday;
+        private static readonly DayOfWeek firstDayOfPreservationWeek = DayOfWeek.Monday;
 
         public static DateTime AddWeeks(this DateTime dateTime, int weeks)
             => dateTime.AddDays(7 * weeks);
 
+        public static string FormatAsYearAndWeekString(this DateTime dateTime)
+            => string.Concat(ISOWeek.GetYear(dateTime).ToString(), "w", ISOWeek.GetWeekOfYear(dateTime).ToString("00"));
+
         public static DateTime StartOfWeek(this DateTime dt)
         {
             var dayOfWeek = GetDayOfWeekMondayAsFirst(dt);
-            var diff = -(dayOfWeek - (int)firstDayOfWeek);
+            var diff = -(dayOfWeek - (int)firstDayOfPreservationWeek);
             return dt.AddDays(diff).Date;
+        }
+
+        public static int GetWeeksUntil(this DateTime fromDateTime, DateTime toDateTime)
+        {
+            var startOfFromWeek = fromDateTime.StartOfWeek();
+            var startOfToWeek = toDateTime.StartOfWeek();
+            var timeSpan = startOfToWeek - startOfFromWeek;
+
+            return timeSpan.Weeks();
         }
 
         private static int GetDayOfWeekMondayAsFirst(DateTime dt)
--- src/tests/Equinor.ProCoSys.Common.Tests/Misc/DateTimeExtensionsTests.cs
+++ src/tests/Equinor.ProCoSys.Common.Tests/Misc/DateTimeExtensionsTests.cs
@@ -2,79 +2,108 @@
 using Equinor.ProCoSys.Common.Misc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Equinor.ProCoSys.Common.Tests.Misc
+namespace Equinor.ProCoSys.Preservation.Domain.Tests
 {
     [TestClass]
     public class DateTimeExtensionsTests
     {
-        private static readonly DateTime Monday = new DateTime(2020, 1, 6, 2, 3, 4, DateTimeKind.Utc);
+        private readonly DateTime _mondayInWeek6 = new DateTime(2020, 2, 3);    // Feb.3
+        private readonly DateTime _fridayInWeek6 = new DateTime(2020, 2, 7);    // Feb.7
+        private readonly DateTime _sundayInWeek6 = new DateTime(2020, 2, 9);    // Feb.9
+        private readonly DateTime _mondayInWeek9 = new DateTime(2020, 2, 24);   // Feb.24
+        private readonly DateTime _fridayInWeek9 = new DateTime(2020, 2, 28);   // Feb.28
+        private readonly DateTime _sundayInWeek9 = new DateTime(2020, 3, 1);    // Mar.1
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsMonday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeSameForAllDaysInSameWeek_FromFriday()
         {
-            var startOfWeek = Monday.StartOfWeek();
+            var expected = 3;
 
-            AssertStartOfWeek(startOfWeek);
+            var weeks = _fridayInWeek6.GetWeeksUntil(_mondayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _fridayInWeek6.GetWeeksUntil(_fridayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _fridayInWeek6.GetWeeksUntil(_sundayInWeek9);
+            Assert.AreEqual(expected, weeks);
         }
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsTuesday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeSameForAllDaysInSameWeek_FromMonday()
         {
-            var tuesday = Monday.AddDays(1);
+            var expected = 3;
 
-            var startOfWeek = tuesday.StartOfWeek();
+            var weeks = _mondayInWeek6.GetWeeksUntil(_mondayInWeek9);
+            Assert.AreEqual(expected, weeks);
 
-            AssertStartOfWeek(startOfWeek);
+            weeks = _mondayInWeek6.GetWeeksUntil(_fridayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _mondayInWeek6.GetWeeksUntil(_sundayInWeek9);
+            Assert.AreEqual(expected, weeks);
         }
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsWednesday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeSameForAllDaysInSameWeek_FromSunday()
         {
-            var wednesday = Monday.AddDays(2);
+            var expected = 3;
 
-            var startOfWeek = wednesday.StartOfWeek();
+            var weeks = _sundayInWeek6.GetWeeksUntil(_mondayInWeek9);
+            Assert.AreEqual(expected, weeks);
 
-            AssertStartOfWeek(startOfWeek);
+            weeks = _sundayInWeek6.GetWeeksUntil(_fridayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _sundayInWeek6.GetWeeksUntil(_sundayInWeek9);
+            Assert.AreEqual(expected, weeks);
         }
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsThursday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeNegative_WhenFromIsAfterTo()
         {
-            var thursday = Monday.AddDays(3);
+            var expected = -3;
 
-            var startOfWeek = thursday.StartOfWeek();
+            var weeks = _sundayInWeek9.GetWeeksUntil(_mondayInWeek6);
+            Assert.AreEqual(expected, weeks);
 
-            AssertStartOfWeek(startOfWeek);
+            weeks = _sundayInWeek9.GetWeeksUntil(_fridayInWeek6);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _sundayInWeek9.GetWeeksUntil(_sundayInWeek6);
+            Assert.AreEqual(expected, weeks);
         }
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsFriday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeSame_WithDaysInSameWeek()
         {
-            var friday = Monday.AddDays(4);
+            var expected = 0;
 
-            var startOfWeek = friday.StartOfWeek();
+            var weeks = _mondayInWeek9.GetWeeksUntil(_mondayInWeek9);
+            Assert.AreEqual(expected, weeks);
 
-            AssertStartOfWeek(startOfWeek);
+            weeks = _mondayInWeek9.GetWeeksUntil(_fridayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _mondayInWeek9.GetWeeksUntil(_sundayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _sundayInWeek9.GetWeeksUntil(_mondayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _sundayInWeek9.GetWeeksUntil(_fridayInWeek9);
+            Assert.AreEqual(expected, weeks);
+
+            weeks = _sundayInWeek9.GetWeeksUntil(_sundayInWeek9);
+            Assert.AreEqual(expected, weeks);
         }
 
         [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsSaturday()
+        public void GetWeeksReferencedFromStartOfWeek_ShouldBeAYear()
         {
-            var saturday = Monday.AddDays(5);
-
-            var startOfWeek = saturday.StartOfWeek();
-
-            AssertStartOfWeek(startOfWeek);
-        }
-
-        [TestMethod]
-        public void StartOfWeek_ShouldGetStartOfWeekWhenDayIsSunday()
-        {
-            var sunday = Monday.AddDays(6);
-
-            var startOfWeek = sunday.StartOfWeek();
-
-            AssertStartOfWeek(startOfWeek);
+            var nextYear = _mondayInWeek9.AddDays(365);
+            var weeks = _mondayInWeek9.GetWeeksUntil(nextYear);
+            Assert.AreEqual(52, weeks);
         }
 
         [TestMethod]
@@ -93,15 +122,36 @@ namespace Equinor.ProCoSys.Common.Tests.Misc
             Assert.AreEqual(DateTimeKind.Utc, dt.Kind);
         }
 
-        private static void AssertStartOfWeek(DateTime dt)
+        [TestMethod]
+        public void FormatAsYearAndWeekString_LastDayOf2019_ShouldGiveFirstWeekOfYearAfter()
         {
-            Assert.AreEqual(Monday.Year, dt.Year);
-            Assert.AreEqual(Monday.Month, dt.Month);
-            Assert.AreEqual(Monday.Day, dt.Day);
-            Assert.AreEqual(0, dt.Hour);
-            Assert.AreEqual(0, dt.Minute);
-            Assert.AreEqual(0, dt.Second);
-            Assert.AreEqual(DateTimeKind.Utc, dt.Kind);
+            var dut = new DateTime(2019, 12, 31);
+
+            Assert.AreEqual("2020w01", dut.FormatAsYearAndWeekString());
         }
+
+        [TestMethod]
+        public void FormatAsYearAndWeekString_FirstDayOf2020_ShouldGiveFirstWeekOfYear()
+        {
+            var dut = new DateTime(2020, 1, 1);
+
+            Assert.AreEqual("2020w01", dut.FormatAsYearAndWeekString());
+        }
+
+        [TestMethod]
+        public void FormatAsYearAndWeekString_LastDayOf2020_ShouldGiveWeek53()
+        {
+            var dut = new DateTime(2020, 12, 31);
+
+            Assert.AreEqual("2020w53", dut.FormatAsYearAndWeekString());
+        }
+
+        [TestMethod]
+        public void StartOfPreservationWeek_ForMonday_ShouldGetMonday()
+            => Assert.AreEqual(DayOfWeek.Monday, _mondayInWeek6.StartOfWeek().DayOfWeek);
+
+        [TestMethod]
+        public void StartOfPreservationWeek_ForSunday_ShouldGetMonday()
+            => Assert.AreEqual(DayOfWeek.Monday, _sundayInWeek6.StartOfWeek().DayOfWeek);
     }
 }
