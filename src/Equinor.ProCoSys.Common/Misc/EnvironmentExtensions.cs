af768da4d4c801d996c239eea53da47e2ae16410
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-16 21:27:51 +0100

    Adding new names for HostEnvironments

--- src/Equinor.ProCoSys.Common/Misc/EnvironmentExtensions.cs
+++ src/Equinor.ProCoSys.Common/Misc/EnvironmentExtensions.cs
@@ -4,6 +4,12 @@ namespace Equinor.ProCoSys.Common.Misc
 {
     public static class EnvironmentExtensions
     {
-        public static bool IsTest(this IHostEnvironment hostEnvironment) => hostEnvironment.EnvironmentName == "Test";
+        public static bool IsTest(this IHostEnvironment hostEnvironment)
+            => hostEnvironment.EnvironmentName == TestEnvironmentName;
+        public static string TestEnvironmentName => "Test";
+
+        public static bool IsIntegrationTest(this IHostEnvironment hostEnvironment)
+            => hostEnvironment.EnvironmentName == IntegrationTestEnvironmentName;
+        public static string IntegrationTestEnvironmentName => "IntegrationTests";
     }
 }
