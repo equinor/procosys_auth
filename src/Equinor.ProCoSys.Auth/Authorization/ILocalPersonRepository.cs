a534dc2a1bb5b610ad5e4d000ab37adcb247c787
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-20 08:49:10 +0100

    check if user exists in local repository before checking cache which get user from ProCoSys

---
+++ src/Equinor.ProCoSys.Auth/Authorization/ILocalPersonRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.Auth.Authorization
+{
+    public interface ILocalPersonRepository
+    {
+        Task<bool> ExistsAsync(Guid userOid);
+    }
+}
