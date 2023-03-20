81d3287697d2b64097c219e7f9e305ee94c5c98f
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-20 10:03:05 +0100

    comment on ILocalPersonRepository

--- src/Equinor.ProCoSys.Auth/Authorization/ILocalPersonRepository.cs
+++ src/Equinor.ProCoSys.Auth/Authorization/ILocalPersonRepository.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Equinor.ProCoSys.Auth.Authorization
 {
+    // Must be implemented and added to DI Container in each
+    // solution using Equinor.ProCoSys.Auth.AuthorizationClaimsTransformation
+    // For solutions without local Person repo, the implemented method should just return false
     public interface ILocalPersonRepository
     {
         Task<bool> ExistsAsync(Guid userOid);
