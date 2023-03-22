05b14c4fcda7242e36a0e0b942bbca284098aa38
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-22 09:15:59 +0100

    rename

--- src/Equinor.ProCoSys.Common/Misc/ConfigurationExtensions.cs
+++ src/Equinor.ProCoSys.Common/Misc/ConfigurationExtensions.cs
@@ -12,12 +12,12 @@ namespace Equinor.ProCoSys.Common.Misc
         public static string GetRequiredConfiguration(this IConfiguration configuration, string key)
             => configuration[key] ?? throw new ArgumentNullException($"Missing configuration {key}");
 
-        public static int GetRequiredIntConfiguration(this IConfiguration configuration, string configKey)
+        public static int GetRequiredIntConfiguration(this IConfiguration configuration, string key)
         {
-            var value = GetRequiredConfiguration(configuration, configKey);
+            var value = GetRequiredConfiguration(configuration, key);
             if (!int.TryParse(value, out var result))
             {
-                throw new ArgumentNullException($"Configuration {configKey} can't be treated as integer");
+                throw new ArgumentNullException($"Configuration {key} can't be treated as integer");
             }
             return result;
         }
