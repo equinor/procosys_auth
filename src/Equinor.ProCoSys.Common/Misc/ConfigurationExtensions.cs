4e202dcfe296be0ec1386d10da732b98cb644c8e
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2023-03-22 09:23:46 +0100

    100998 - Common 1.0.7. ConfigurationExtensions (#13)
    * Common 1.0.7. ConfigurationExtensions

---
+++ src/Equinor.ProCoSys.Common/Misc/ConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Equinor.ProCoSys.Common.Misc
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetRequiredConnectionString(this IConfiguration configuration, string name)
+            => configuration.GetConnectionString(name)
+               ?? throw new ArgumentNullException($"Missing connectionstring in configuration {name}");
+
+        public static string GetRequiredConfiguration(this IConfiguration configuration, string key)
+            => configuration[key] ?? throw new ArgumentNullException($"Missing configuration {key}");
+
+        public static int GetRequiredIntConfiguration(this IConfiguration configuration, string key)
+        {
+            var value = GetRequiredConfiguration(configuration, key);
+            if (!int.TryParse(value, out var result))
+            {
+                throw new ArgumentNullException($"Configuration {key} can't be treated as integer");
+            }
+            return result;
+        }
+    }
+}
