540d793b2eb95caf76c21749c767b284f4f3b33d
Elisabeth Bratli <ELIBRA@equinor.com>
2023-11-28 08:57:16 +0100

    remove unused using

--- src/Equinor.ProCoSys.Common/Email/EmailValidator.cs
+++ src/Equinor.ProCoSys.Common/Email/EmailValidator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Net.Mail;
 using System.Text.RegularExpressions;
 
 namespace Equinor.ProCoSys.Common.Email;
